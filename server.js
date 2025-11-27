87fd170d0fb66c3724f7feba55c73bdd75460cad
AutisticDenominator <AutisticDenominator@proton.me>
2025-11-27 19:58:56 +0100

    Search function base

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const http = require('http');
 const port = process.env.PORT || 1337;
 const fs = require('fs')
+const utils = require('./utils')
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    let aux_url = req.url[0] + req.url[1] + req.url[2] + req.url[3] + req.url[4] + req.url[5];
 
     if(req.url == '/'){
         let html = fs.readFileSync('./frontend/index.html');
@@ -15,6 +17,32 @@ http.createServer(function (req, res) {
     }else if(req.url == '/search'){
         let html = fs.readFileSync('./frontend/search.html');
         res.write(html); 
+    }else if(aux_url == '/query'){
+        let html = fs.readFileSync('./frontend/index.html');
+        res.write(html);
+        
+        let state = 0;
+        let values = new Object({'filter-check' : 'off'});
+        let temp = '';
+        let temp_name = '';
+
+        for(let i = 7; i < length(req.url); i++){
+            if(req.url == '&' || req.url == '='){
+                state = state + 1;
+
+                if((state % 2) == 0){
+                    temp_name = temp;
+                }else{
+                    values[temp_name] = temp;
+                }
+
+                temp = '';
+            }else{
+                temp = temp + req.url[i];
+            }
+        }
+
+        utils.Search(values['keyword'], values['filtercheck'], values['filter']);
     }
 
     res.end();
---
+++ utils.js
@@ -0,0 +1,14 @@
+function Search(keyword, filter_check, filter){
+    if(filter_check == 'on'){
+
+    }else{
+
+    }
+    return
+}
+
+function Create(title, content, filter_check, filter){
+    return;
+}
+
+module.exports = {Search, Create};
